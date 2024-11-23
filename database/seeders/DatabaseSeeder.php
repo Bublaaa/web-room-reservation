206f92e76fca855dcc3a1ede43463459a59f5f53
Bublaaa <yanuarkrisna7@gmail.com>
2024-11-23 14:46:03 +0700

    Add role coloumn and seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,9 +17,28 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // Creating admin user
+        User::create([
+            'username' => 'administrator',
+            'email' => 'admin@gmail.com',
+            'role' => 'admin',
+            'email_verified_at' => now(),
+            'password' => Hash::make('12345678'),
+            'remember_token' => null,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        // Creating regular user
+        User::create([
+            'username' => 'user',
+            'email' => 'user@gmail.com',
+            'role' => 'user',
+            'email_verified_at' => now(),
+            'password' => Hash::make('12345678'),
+            'remember_token' => null,
+            'created_at' => now(),
+            'updated_at' => now(),
         ]);
     }
 }
