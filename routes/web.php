591a00efc933bf8c12eca43c02e6c8f6abc3f515
Bublaaa <yanuarkrisna7@gmail.com>
2024-11-24 10:41:08 +0700

    Add role middleware

--- routes/web.php
+++ routes/web.php
@@ -7,47 +7,33 @@ use App\Http\Controllers\RoomController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ReservationController;
 
+// Guest routes
 Route::middleware('guest')->group(function () {
-    Route::get('/',[WebAuthenticationController::class, 'showLoginForm'])->name('showLoginForm');
+    Route::get('/', [WebAuthenticationController::class, 'showLoginForm'])->name('showLoginForm');
     Route::post('/login', [WebAuthenticationController::class, 'login'])->name('login');
     Route::get('/register', [WebAuthenticationController::class, 'showRegister'])->name('register');
     Route::post('/register', [WebAuthenticationController::class, 'register']);
 });
 
 Route::middleware('auth')->group(function () {
-    // Logout route
-    Route::post('/logout', [WebAuthenticationController::class, 'logout'])->name('logout');
-    // admin route
-    Route::prefix('admin')->group(function () {
-        Route::get('/dashboard', [AdminController::class, 'showAdminDashboard'])->name('admin.dashboard');
-        Route::get('/rooms', [AdminController::class, 'showRoomDashboard'])->name('admin.room.dashboard');
-        Route::get('/reservations', [AdminController::class, 'showReservationDashboard'])->name('admin.reservation.dashboard');
-        Route::get('/users', [AdminController::class, 'showUserDashboard'])->name('admin.user.dashboard');
-        
-        Route::get('/register', [AdminController::class, 'register'])->name('register-user');
+    // Admin routes
+    Route::prefix('admin')->middleware('role:admin')->as('admin.')->group(function () {
+        Route::get('/dashboard', [AdminController::class, 'showAdminDashboard'])->name('dashboard');
+        Route::get('/rooms', [AdminController::class, 'showRoomDashboard'])->name('room.dashboard');
+        Route::get('/reservations', [AdminController::class, 'showReservationDashboard'])->name('reservation.dashboard');
+        Route::get('/users', [AdminController::class, 'showUserDashboard'])->name('user.dashboard');
+
+        // Resource routes
         Route::resource('room', RoomController::class);
         Route::resource('reservation', ReservationController::class);
+        
     });
-    // user route
-    Route::middleware('role:user')->group(function () {
-        Route::get('/user/dashboard', function () {
-            return view('user.dashboard');
-        })->name('user.dashboard');
+
+    // User routes
+    Route::prefix('user')->middleware('role:user')->as('user.')->group(function () {
+        Route::get('/dashboard', [UserController::class, 'showUserDashboard'])->name('dashboard');
     });
-});
 
-Route::prefix('admin')->middleware(['auth'])->group(function () {
-    Route::get('/dashboard', [AdminController::class, 'showAdminDashboard'])->name('admin.dashboard');
-    Route::get('/rooms', [AdminController::class, 'showRoomDashboard'])->name('admin.room.dashboard');
-    Route::get('/reservations', [AdminController::class, 'showReservationDashboard'])->name('admin.reservation.dashboard');
-    Route::get('/users', [AdminController::class, 'showUserDashboard'])->name('admin.user.dashboard');
-    
-    Route::get('/register', [AdminController::class, 'register'])->name('register-user');
-    Route::resource('room', RoomController::class);
-    Route::resource('reservation', ReservationController::class);
-
-});
-
-Route::prefix('user')->middleware(['auth'])->group(function () {
-    Route::get('/dashboard', [UserController::class, 'showUserDashboard'])->name('user.dashboard');
+    // Logiut route
+    Route::post('/logout', [WebAuthenticationController::class, 'logout'])->name('logout');
 });
